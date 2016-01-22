45c04a5bc1a66b442e858db973ae693233255b46
Max Iakovyna <max.yakovina@gmail.com>
2016-01-22 12:31:38 +0200

    ES node config refactored

--- supervisor/src/main/scala-2.11/dal/elasticsearch/ElasticMetricsDAL.scala
+++ supervisor/src/main/scala-2.11/dal/elasticsearch/ElasticMetricsDAL.scala
@@ -68,7 +68,7 @@ object ElasticSearchInstance {
 
   val settings = Settings.settingsBuilder()
   settings.put("network.host", "0.0.0.0")
-  val node = NodeBuilder.nodeBuilder().local(false).settings(settings).node()
+  val node = NodeBuilder.nodeBuilder().local(true).settings(settings).node()
 
   sys addShutdownHook { node.close() }
 
